ff559f788eff4c3c1395d41288dd6fa0868538b2
Campbell Cole <cmcoleco@gmail.com>
2020-09-21 02:28:46 -0700

    canvas testing

--- public/canvas.js
+++ public/canvas.js
@@ -1,14 +1,44 @@
-// TOKEN:
-// 7774~sTvVFoEkjCPobx7S5Na4hElw9BS5fzOxHj4v16HZ50wi1dTlI4697BXn5OT5zAGI
-
 const fetch = require('node-fetch');
+require('dotenv').config();
 
-const TOKEN = '7774~sTvVFoEkjCPobx7S5Na4hElw9BS5fzOxHj4v16HZ50wi1dTlI4697BXn5OT5zAGI';
+const TOKEN = process.env.ACCESS_TOKEN;
+console.log(TOKEN);
 
 const URL = (endpoint) => `https://4cd.instructure.com/api/v1/${endpoint}?access_token=${TOKEN}`;
 
-fetch(URL('courses')).then(res => res.json()).then((data) => {
-  data.forEach(course => {
-    console.log(course.calendar.ics)
+const canv = (endpoint) => fetch(URL(endpoint)).then(res => res.json());
+
+var potentialTasks = {};
+console.log('fetching courses')
+fetch(URL('courses')).then(res => res.json()).then((courses) => {
+  var count = 0;
+  courses.forEach(course => {
+    potentialTasks[course.id] = [];
+    console.log('fetching assignments for course ' + course.id);
+    canv(`courses/${course.id}/assignments`).then((assignments) => {
+      pushAll(potentialTasks[course.id], assignments);
+      canv(`courses/${course.id}/modules`).then((modules) => {
+        var mcount = 0;
+        modules.forEach(module => {
+          canv(`courses/${course.id}/modules/${module.id}/items`).then(items => {
+            mcount++;
+            pushAll(potentialTasks[course.id], items);
+            if (mcount === modules.length) {
+              count++;
+              console.log(`fetched course ${course.id}`);
+            }
+            if (count === courses.length) done();
+          });
+        });
+      });
+    });
   });
-})
+});
+
+function pushAll(arr, items) {
+  arr.push.apply(arr, items);
+}
+
+function done() {
+  console.log(potentialTasks['57557'].length);
+}
