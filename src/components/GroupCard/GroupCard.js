f465d5be1dd1bbd71e8ba60ac200b7481d24ee33
campbellcole <cmcoleco@gmail.com>
2020-10-07 20:37:38 -0700

    attempt at css only collapse

--- src/components/GroupCard/GroupCard.js
+++ src/components/GroupCard/GroupCard.js
@@ -5,18 +5,23 @@ import './GroupCard.scss'
 export default class GroupCard extends Component {
   constructor() {
     super()
-    this.state = { clickedTask: undefined }
+    this.state = { clickedTask: undefined, collapsed: false }
+    this.handleCollapse = this.handleCollapse.bind(this)
   }
   render() {
+    const col = this.state.collapsed ? 'collapsed' : ''
     return (
-      <div className='group-card'>
-        <div className='group-header'>{this.props.title}</div>
-        <ul className='group-tasks'>
+      <div className={`group-card ${col}`}>
+        <div className={`group-header ${col}`} onClick={this.handleCollapse}>{this.props.title}</div>
+        <ul className={`group-tasks ${col}`}>
           { this.props.tasks.map(task => (
-              <li className='group-task' onClick={this.props.handleTaskClick(task)} key={task.id}><MdChevronRight />{task.title}</li>
+              <li className={`group-task ${col}`} onClick={this.props.handleTaskClick(task)} key={task.id}><MdChevronRight />{task.title}</li>
           ))}
         </ul>
       </div>
     )
+  }
+  handleCollapse() {
+    this.setState({collapsed: !this.state.collapsed})
   }
 }
